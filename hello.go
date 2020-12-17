0f813243a7c79a1de4fc0533e168d912567c6e0e
Roshni-Joseph <roshnijosepht@gmail.com>
2020-12-17 17:14:22 +0530

    task 1_print name,id,mark

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Roshni Joseph"
+	var id int = 5
+	var mark float32 = 3.5
+
+	fmt.Printf("My name is %s and ID is %d also mark is %f", name, id, mark)
+}
